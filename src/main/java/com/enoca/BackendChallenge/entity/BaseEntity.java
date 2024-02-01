1c08a3bbdc056d9a6dbb3a16287bebfb106354c4
Mehmet <mehmetsonmez216@gmail.com>
2024-02-01 14:27:37 +0300

    Added getter and setter to baseEntity

--- src/main/java/com/enoca/BackendChallenge/entity/BaseEntity.java
+++ src/main/java/com/enoca/BackendChallenge/entity/BaseEntity.java
@@ -1,9 +1,13 @@
 package com.enoca.BackendChallenge.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity{
 
     @Id
