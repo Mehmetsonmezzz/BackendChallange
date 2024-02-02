506712d59670f4300866ea6bb77ee0e45202f1b1
Mehmet <mehmetsonmez216@gmail.com>
2024-02-03 02:24:48 +0300

    Order entity added

---
+++ src/main/java/com/enoca/BackendChallenge/entity/Order.java
@@ -0,0 +1,31 @@
+package com.enoca.BackendChallenge.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "order",schema = "challenge")
+public class Order extends BaseEntity{
+
+
+    @Column(name = "total_amount")
+    private double totalAmount;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name="customer_id")
+    private Customer customer;
+
+   /*
+*/
+}
