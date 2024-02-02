3903a3ce465f8d9a0e4e68eaf242ab72051c6c41
Mehmet <mehmetsonmez216@gmail.com>
2024-02-02 22:57:21 +0300

    added createdDate and updatedDate to BaseEntity

--- src/main/java/com/enoca/BackendChallenge/entity/BaseEntity.java
+++ src/main/java/com/enoca/BackendChallenge/entity/BaseEntity.java
@@ -3,6 +3,11 @@ package com.enoca.BackendChallenge.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 
 @MappedSuperclass
@@ -14,4 +19,14 @@ public abstract class BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private LocalDateTime createdDate ;
+
+
+
+    @UpdateTimestamp
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
 }
